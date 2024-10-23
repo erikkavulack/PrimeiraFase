a195bff3c29bf1fd435be739150869d3e7991d6b
erikkavulack <kavulacke@gmail.com>
2024-10-22 22:20:30 -0300

    22/10

--- Projetos Erik/login/script.js
+++ Projetos Erik/login/script.js
@@ -1,24 +1,59 @@
 let bancoDeDados = [];
-function add(){
+function add() {
     let usuario = document.getElementById("username").value;
-    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
-    if(pos == -1){
+    if (existe(usuario) == false) {
         bancoDeDados.push(usuario.toUpperCase());
         alert("Usuário cadastrado com sucesso!");
-        document.getElementById("username").value = null  
-    } 
+        document.getElementById("username").value = null
+    }
     else {
         alert("Esse usuário já foi cadastrado!")
     }
 }
 
-function login(){
+function login() {
     let usuario = document.getElementById("user").value;
-    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
-    if(pos == -1){
+    if (existe(usuario) == false) {
         alert("usuário não cadastrado!")
     } else {
         alert("Bem vindo ao sitema!");
-        document.getElementById("user").value = null 
+    }
+    document.getElementById("user").value = null
+}
+
+function remove() {
+    let usuario = document.getElementById("userDelete").value;
+    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
+    if (pos == -1) {
+        alert("usuário não cadastrado!");
+    } 
+    else {
+        bancoDeDados.splice(pos, 1);
+        alert("Usuário excluído com sucesso!");
+    }
+    document.getElementById("userDelete").value = null;
+}
+
+function edit() {
+    let usuario = document.getElementById("userEdit").value;
+    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
+    if(pos == -1){
+        alert("Usuário não encontrado no banco de dados!");
+    }
+    else {
+        let novoValor = prompt("Digite o novo valor").toUpperCase();
+        bancoDeDados[pos] = novoValor;
+        alert("Usuário alterado com sucesso!");
+    }
+    document.getElementById("userEdit").value = null;
+}
+
+function existe(username){
+    let pos = bancoDeDados.indexOf(username.toUpperCase());
+    if(pos == -1){
+        return false;
+    }
+    else{
+        return true;
     }
 }
