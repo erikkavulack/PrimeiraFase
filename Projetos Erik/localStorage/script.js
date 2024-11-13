36008f3034f0c4be16d5de3d4d19f505f43dee93
erikkavulack <kavulacke@gmail.com>
2024-11-12 22:13:44 -0300

    12/11

---
+++ Projetos Erik/localStorage/script.js
@@ -0,0 +1,55 @@
+const campoLogin = document.getElementById("login");
+const campoSenha = document.getElementById("password");
+const campoNovoLogin = document.getElementById("novoLogin");
+const campoNovaSenha = document.getElementById("novaSenha");
+const campoRepSenha = document.getElementById("repSenha");
+
+function login(){
+    let login = campoLogin.value;
+    let senha = campoSenha.value;
+    let mensagem = "Usuário ou senha incorreta!";
+    let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+    if (bancoDeDados == null) {
+        mensagem = "Nenhum usuário cadastrado até o momento";
+    } else {
+        for (let usuario of bancoDeDados) {
+            if (usuario.login == login && usuario.senha == senha) {
+                mensagem = "Parabéns, você logou!";
+                localStorage.setItem("logado", JSON.stringify(usuario));
+                window.location.href = "home.html";
+                break;
+            }
+        }        
+    }
+    alert(mensagem);
+}
+
+function  cadastra(){
+    if (campoNovaSenha.value == campoRepSenha.value) {
+        const usuario = {
+            login: campoNovoLogin.value,
+            senha: campoNovaSenha.value
+        };
+        let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+        if (bancoDeDados == null) {
+            bancoDeDados = [];
+        }
+        bancoDeDados.push(usuario);
+        localStorage.setItem("bancoDeDados", JSON.stringify(bancoDeDados));
+        alert("Usuário cadastrado com sucesso!");
+    } else {
+        alert("As senhas são diferentes!");
+    }
+    
+}
+
+function deslogar(){
+    localStorage.setItem("logado", JSON.stringify(null));
+    window.location.href = "index.html";
+}
+
+function carrega(){
+    let usuarioLogado = JSON.parse(localStorage.getItem("logado"));
+    let nome = usuarioLogado.login;
+    document.getElementById("nome").innerHTML = nome;
+}
